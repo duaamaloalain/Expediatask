ac06ec5f865f6c263d2723609678c7b23a7deacf
duaamaloalain <maloalain.duaa94@hotmail.com>
2017-04-09 19:29:52 +0300

    favicon

--- config/routes.js
+++ config/routes.js
@@ -1,20 +1,14 @@
 var request = require('request');
 var path = require('path');
 
-var obj={};
 module.exports = function (app, express) {
 	app.get('/api/expedia',function(req,res) { //get data from API
 		request('https://offersvc.expedia.com/offers/v2/getOffers?scenario=deal-finder&page=foo&uid=foo&productType=Hotel', function(error, response, body) {
-			var Data=JSON.parse(body); // to put all the data insid Data
+			var Data=JSON.parse(body); //to put all the data insid Data
 			if(error){
 				throw error;
 			}else {		
-				for(var key in Data){ 
-					if(obj[key]===undefined){
-						obj[key]=Data[key];
-					}    
-				}
-				res.json(obj);//response the json obj
+				res.json(Data); //response the json obj
 			}
 		});
 	});    

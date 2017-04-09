7b1f4e7de436f5b6dc23a9c59bda00638a9f5b50
duaamaloalain <maloalain.duaa94@hotmail.com>
2017-04-09 16:22:30 +0300

    connect backend with frontend

--- client/app.js
+++ client/app.js
@@ -1,10 +1,10 @@
 angular.module("myApp",[])
 .controller("AllExpediaData",function($scope,$http){
 	var object;
-    $http.get('/api/expedia').then(function(expediaData){
-       object=expediaData;
+    $http.get('/api/expedia').then(function(expediaData){ //get data from backend by api
+       object=expediaData; 
        for(var key in object){
-         $scope.AllData=object.data.offers.Hotel; 
+         $scope.AllData=object.data.offers.Hotel;  
        }
     })
 })

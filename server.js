9b4c955b03f269e69d15a8afeddcafdf9fef23ff
duaamaloalain <maloalain.duaa94@hotmail.com>
2017-04-09 16:17:25 +0300

    done server

--- server.js
+++ server.js
@@ -4,10 +4,10 @@ var port = process.env.PORT || 3000;
 var app = express();
 
 
-require('./config/middleware.js') (app,express); 
-require('./config/routes.js') (app,express); 
+require('./config/middleware.js') (app,express); //middleware have body parser and morgan 
+require('./config/routes.js') (app,express); // get data from api
 
-
+// open server
 app.listen(port, function () {
   console.log(' app listening on port '+ port);
 })
